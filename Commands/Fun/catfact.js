7f0ac6f11e360063ff7df3706268917131cf8fd9
Glitch (fortunate-teal-bottle) <none>
2022-10-12 10:34:39 +0000

    💑🔋 Checkpoint ./Commands/Fun/catfact.js:27621106/79

--- Commands/Fun/catfact.js
+++ Commands/Fun/catfact.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('catfact')
-    .setDescription(`Same as dogfact, except it's for cats.`),
+    .setDescription(`Get a fact about cats`),
   async execute(interaction) {
     const response = await fetch('https://catfact.ninja/facts?limit=1&max_length=140%27');
     const data = await response.json();
